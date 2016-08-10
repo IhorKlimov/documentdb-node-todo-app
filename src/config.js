9543caf6255762cfd5c4e0401baf1ed24f13b19c
Ihor Klimov <iklimov6@gmail.com>
2016-08-10 15:43:45 +0200

    keys

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "[URI for DocumentDB endpoint]";
-config.authKey = process.env.AUTH_KEY || "[Master key for DocumentDB]";
+config.host = process.env.HOST || "https://botdemogit.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "4wgLxlby19yTepqE7lOFTASGrYwjpHLckS0e8OA4DTLj5Sk8XnfZj3qmXDWwueRXAnFdbyYznq0IM0tdSCf4GA==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
